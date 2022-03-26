731419939bef6e2678138345ce42db078488783f
Joan Gerard <joan.gerard@outlook.com>
2022-03-26 13:04:43 +0100

    project structure and initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { CssBaseline, Grid } from '@material-ui/core';
 
-function App() {
+import { Header } from './components/Header/Header';
+import { List } from './components/List/List';
+import { Map } from './components/Map/Map';
+
+export const App = () => {
   return (
-    <div className="App">Hi
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
-
-export default App;
