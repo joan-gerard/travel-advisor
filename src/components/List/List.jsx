9d41a6da01c5cc0ce400955db2e46b78ada486d0
Joan Gerard <joan.gerard@outlook.com>
2022-03-26 15:29:54 +0100

    List and PlaceDetails components

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core'
+import {PlaceDetails} from '../PlaceDetails/PlaceDetails'
+import useStyles from './styles.js'
+
 
 export const List = () => {
+
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants')
+  const [rating, setRating] = useState('')
+
+  const places = [
+    {name: 'Cool Place'},
+    {name: 'Best Beer'},
+    {name: 'Best Steak'},
+    {name: 'Cool Place'},
+    {name: 'Best Beer'},
+    {name: 'Best Steak'}
+  ]
+
   return (
-    <div>List</div>
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants and Hotels around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3 stars</MenuItem>
+          <MenuItem value={4}>Above 4 stars</MenuItem>
+          <MenuItem value={4.5}>Above 4.5 stars</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, idx) => (
+          <Grid item key={idx} xs={12}>
+            <PlaceDetails place={place}/>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   )
 }
 
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const PlaceDetails = () => {
+export const PlaceDetails = ({place}) => {
   return (
-    <div>PlaceDetails</div>
+    <div>{place.name}</div>
   )
 }
 
